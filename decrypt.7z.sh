32e0baf055ec39bc494999e072f196ad3143e0c5
Ron DeMara <rondemara@gmail.com>
2014-06-18 22:59:03 -0400

    I have no idea what I'm doing...

--- decrypt.7z.sh
+++ decrypt.7z.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 read -s -p "Enter Password: " password
-encrypted_pass=`(echo '$password' | sha256sum) | awk '{print $1}'`
+encrypted_pass=$password
 7z e -p$encrypted_pass $1
 
--- encrypt.7z.sh
+++ encrypt.7z.sh
@@ -5,7 +5,7 @@ read -s -p "Enter Password again: " passwordagain
 echo ""
 
 if [ $password == $passwordagain ]; then
-   encrypted_pass=`(echo '$password' | sha256sum) | awk '{print $1}'`
+   encrypted_pass=$password
    7z a -mhe=on -p$encrypted_pass $1.enc $1
    rm -f $1
 else
