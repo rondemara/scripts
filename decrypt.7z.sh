64895a90405895f10d73c94d186b9507074800e0
Ron DeMara <rondemara@gmail.com>
2014-06-22 17:42:17 -0400

    Updated scripts to use zenity messages.

--- decrypt.7z.sh
+++ decrypt.7z.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-read -s -p "Enter Password: " password
+#read -s -p "Enter Password: " password
+password=`zenity --password --title "Enter Encryption Password:"`
 encrypted_pass=$password
 7z e -p$encrypted_pass $1
 
--- encrypt.7z.sh
+++ encrypt.7z.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-read -s -p "Enter Password: " password
-echo ""
-read -s -p "Enter Password again: " passwordagain
-echo ""
+
+password=`zenity --password --title "Enter Encryption Password:"`
+passwordagain=`zenity --password --title "Enter Encryption Password Again:"`
 
 if [ $password == $passwordagain ]; then
    encrypted_pass=$password
    7z a -mhe=on -p$encrypted_pass $1.enc $1
    rm -f $1
 else
-   echo "Passwords do not match..."
+   zenity --error --text="Password is incorrect."
 fi
 
 
